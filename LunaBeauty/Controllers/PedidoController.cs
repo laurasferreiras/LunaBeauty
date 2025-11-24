9833d40160f6ed03c4253618d121d68eaa4ee24a
laurasferreiras <laurasferreiras6@gmail.com>
2025-11-23 22:10:56 -0300

    Alterações básicas

--- LunaBeauty/Controllers/PedidoController.cs
+++ LunaBeauty/Controllers/PedidoController.cs
@@ -81,16 +81,17 @@ namespace LunaBeauty.Controllers
                     // Calcula o total do item
                     item.CalcularValorTotal();
 
+
                     // --- BAIXA DE ESTOQUE (NOVO) ---
                     if (item.ProdutoOrigem != null)
                     {
                         item.ProdutoOrigem.Estoque -= item.Quantidade;
 
                         // Garante que não fica negativo (boa prática de governança de dados)
-                        if (item.ProdutoOrigem.Estoque < 0)
-                            item.ProdutoOrigem.Estoque = 0;
+                        
 
-                        _context.Update(item.ProdutoOrigem);
+
+                            _context.Update(item.ProdutoOrigem);
                     }
                 }
 
